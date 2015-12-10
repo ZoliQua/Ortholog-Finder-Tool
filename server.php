e7a7f00142e4971c07a6227a8e404ddbfd61aae6
Zoltan Dul <ssdbtc@gmail.com>
2015-12-10 23:01:15 +0000

    frissites

---
+++ server.php
@@ -0,0 +1,31 @@
+<?php
+
+/* (c) Dúl Zoltán 2012 */
+/* ADATBÁZISOKBÓL LEKÉRŐ adott ID alapján */
+/* Hibák mutatása és futési  php beállítása */
+
+error_reporting(E_ALL);
+ini_set('display_errors', 'on');
+ini_set('memory_limit', '-1');
+ini_set('max_execution_time', '-1');
+
+/* Kézzel az összes fájl behívása */
+
+print_r($_SERVER);
+
+print "</textarea>\n<BR>";
+print "! GAME OVER !";
+
+print phpinfo(32);
+?>
+
+	+===+===+===+
+ 	|	|	|	|
+ 	|	|	|	|
+ 	+===+===+===+
+ 	|	|	|	|
+ 	|	|	|	|
+ 	+===+===+===+
+ 	|	|	|	|
+ 	|	|	|	|
+ 	+===+===+===+
